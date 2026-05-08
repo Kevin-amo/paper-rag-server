0686f6b110498ca68f3b2b82d3402d9778ba279a
Kevin-amo <LQR0430@outlook.com>
2026-05-08 11:53:02 +0800

    环境配置

---
+++ src/main/java/paperragserver/PaperRagServerApplication.java
@@ -0,0 +1,13 @@
+package paperragserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperRagServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperRagServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/paperragserver/PaperRagServerApplicationTests.java
@@ -0,0 +1,13 @@
+package paperragserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperRagServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
